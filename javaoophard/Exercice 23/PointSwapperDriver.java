9a496de7e85fc7820cc8e0bc8efd8e689d265fc5
abdessamadG <abdessamadgamraoui@gmail.com>
2024-09-20 01:15:44 +0100

    Writing a Silly Class with Generics

---
+++ javaoophard/Exercice 23/PointSwapperDriver.java
@@ -0,0 +1,44 @@
+public class PointSwapperDriver {
+	public static void main( String [] args ) {
+		PointSwapperInt pi = new PointSwapperInt(3, 5);
+
+		System.out.println( "before: " + pi );
+		pi.swap();
+		System.out.println( "after: " + pi );
+		pi.swap();
+		System.out.println( "after after: " + pi );
+
+		PointSwapperDouble pd = new PointSwapperDouble(1.1, 4.4);
+
+		System.out.println( "\nbefore: " + pd );
+		pd.swap();
+		System.out.println( "after: " + pd );
+		pd.swap();
+		System.out.println( "after after: " + pd );
+
+		PointSwapper<Integer> pg1 = new PointSwapper<>(2, 6);
+		PointSwapper<Double> pg2 = new PointSwapper<>(1.3, 5.7);
+
+		System.out.println( "\nbefore: " + pg1 );
+		pg1.swap();
+		System.out.println( "after: " + pg1 );
+		pg1.swap();
+		System.out.println( "after after: " + pg1 );
+
+		System.out.println( "\nbefore: " + pg2 );
+		pg2.swap();
+		System.out.println( "after: " + pg2 );
+		pg2.swap();
+		System.out.println( "after after: " + pg2 );
+			
+		boolean ab = false;
+		if ( pi == pi && pd == pd && pg1 == pg1 && pg2 == pg2 ) {
+			 ab = true;
+			 System.out.println( ab );
+		}
+		else {
+			ab = false;
+			System.out.println( ab );
+		}
+	}
+}
